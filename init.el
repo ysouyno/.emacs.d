794898f37ba9e0fe022c437d7d14b7ffe0e639a0
ysouyno <ysouyno@163.com>
2024-03-25 09:45:24 +0800

    For tree-sitter

--- init.el
+++ init.el
@@ -31,6 +31,14 @@
 (setq recentf-max-saved-items 100)
 (setq recentf-max-menu-items 100)
 (global-set-key "\C-c\ f" 'recentf-open-files)
+
+(setq major-mode-remap-alist
+      '((sh-mode . bash-ts-mode)
+        (c-mode . c-ts-mode)
+        (c++-mode . c++-ts-mode)
+        (c-or-c++-mode . c-or-c++-ts-mode)
+        (python-mode . python-ts-mode)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
