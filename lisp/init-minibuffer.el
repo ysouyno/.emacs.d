275120e5e5424c1eaca6671bdb85386cfeb753bb
ysouyno <ysouyno@163.com>
2024-10-18 22:07:22 +0800

    设置 minibuffer 路径上的退格回到父目录

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -5,6 +5,19 @@
 (when (maybe-require-package 'vertico)
   (add-hook 'after-init-hook 'vertico-mode))
 
+;; Configure directory extension.
+;; 设置 minibuffer 路径上的退格回到父目录
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
 (when (maybe-require-package 'marginalia)
   (add-hook 'after-init-hook 'marginalia-mode))
 
