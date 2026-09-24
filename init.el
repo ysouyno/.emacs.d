ecee26b3087d0e0cbf6255b0f4fb85fd52217c8e
ysouyno <ysouyno@163.com>
2026-09-24 09:48:04 +0800

    将 init.el custom.el 中的个人配置转移到 init-custom.el 中

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 (require 'init-rust)
 (require 'init-markdown)
 
+
 (when (and (require 'treesit nil t)
            (fboundp 'treesit-available-p)
            (treesit-available-p)
@@ -30,56 +31,7 @@
   (require 'init-cpp))
 
 
-;;; Save command history
-(add-hook 'after-init-hook 'savehist-mode)
-
-
-;;; Save cursor position
-;; http://xahlee.info/emacs/emacs/emacs_save_cursor_position.html
-(cond
- ((< emacs-major-version 25)
-  (require 'saveplace)
-  (setq-default save-place t))
- ((>= emacs-major-version 25)
-  (save-place-mode 1)))
-
-
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(line-number-mode t)
-(column-number-mode t)
-;; (size-indication-mode t)
-
-
-;;; Personal settings
-;; org 模式下设置自动断行
-(add-hook 'org-mode-hook (lambda ()
-                           (setq-local truncate-lines nil)))
-
-;; 正在编辑的文件内容有修改时自动更新
-(global-auto-revert-mode)
-
-;; 鼠标滚动时保持光标相对位置不变
-(setq scroll-preserve-screen-position 'always)
-(global-hl-line-mode +1)
-
-;; 以下后缀名使用 glsl-mode
-(use-package glsl-mode
-  :ensure t
-  :config
-  (setq auto-mode-alist
-        (append
-         '(("\\.vs\\'" . glsl-mode))
-         '(("\\.fs\\'" . glsl-mode))
-         '(("\\.gs\\'" . glsl-mode))
-         auto-mode-alist)))
-
-;; 关闭 C-M-\ 时自动插入 tab 字符
-(setq-default indent-tabs-mode nil)
-
-;; 右键打开带中文文件名的乱码问题
-(when (eq system-type 'windows-nt)
-  (setq file-name-coding-system 'gb18030))
+(require 'init-custom)
 
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
